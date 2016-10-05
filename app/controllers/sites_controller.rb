faf9f229175305c0ede856871990adef8bf32153
Stephanie Gurung <stephr3@gmail.com>
2016-10-05 15:02:16 -0700

    search map by zip code or city

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,16 @@
 class SitesController < ApplicationController
   def index
+    if params[:center]
+      center = params[:center]
+      lat = Geocoder.search(center).first.coordinates.first
+      lng = Geocoder.search(center).first.coordinates.last
+      @center_on = "{lat: #{lat}, lng: #{lng}}"
+      @zoom = 13
+    else
+      @center_on = "{lat: 45.543897, lng: -122.655977}"
+      @zoom = 9
+    end
+
     if params[:site_type].present?
       if params[:site_type] == 'church'
         @layer = "layer_churches"
