6ce4ca6e976dd244f0fbde4b34d114986957c29c
Stephanie Gurung <stephr3@gmail.com>
2016-10-05 13:37:08 -0700

    filter map by class type

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,7 +1,26 @@
 class SitesController < ApplicationController
   def index
+    if params[:site_type].present?
+      if params[:site_type] == 'church'
+        @layer = "layer_churches"
+      elsif params[:site_type] == 'college'
+        @layer = "layer_colleges"
+      elsif params[:site_type] == 'business'
+        @layer = "layer_businesses"
+      elsif params[:site_type] == 'library'
+        @layer = "layer_libraries"
+      elsif params[:site_type] == 'public_school'
+        @layer = "layer_schools"
+      elsif params[:site_type] == 'social_service'
+        @layer = "layer_services"
+      else
+        @layer = "layer_all"
+      end
+    else
+      @layer = "layer_all"
+    end
   end
-  
+
   def north_northeast
   end
 
