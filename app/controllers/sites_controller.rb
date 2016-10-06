809255addae6dd830297ad3b8c37be9626287b44
Stephanie Gurung <stephr3@gmail.com>
2016-10-05 20:18:15 -0700

    limit map search to zip code only

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,11 +1,17 @@
 class SitesController < ApplicationController
   def index
     if params[:center]
-      center = params[:center]
-      lat = Geocoder.search(center).first.coordinates.first
-      lng = Geocoder.search(center).first.coordinates.last
-      @center_on = "{lat: #{lat}, lng: #{lng}}"
-      @zoom = 13
+      @center = params[:center]
+      if (@center.is_number?) and (0 < @center.length) and (@center.length < 6)
+        lat = Geocoder.search(@center).first.coordinates.first
+        lng = Geocoder.search(@center).first.coordinates.last
+        @center_on = "{lat: #{lat}, lng: #{lng}}"
+        @zoom = 13
+      else
+        flash[:alert] = "Please enter a valid zip code."
+        @center_on = "{lat: 45.543897, lng: -122.655977}"
+        @zoom = 9
+      end
     else
       @center_on = "{lat: 45.543897, lng: -122.655977}"
       @zoom = 9
