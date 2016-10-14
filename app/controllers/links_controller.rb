ec1b19bad61a5f54b72db7c7d2d5911783fe4889
Stephanie Gurung <stephr3@gmail.com>
2016-10-14 12:00:20 -0700

    allow admin to edit or delete a link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,17 +1,23 @@
 class LinksController < ApplicationController
-  before_filter :is_admin, only:[:new]
+  before_filter :is_admin, only:[:new, :edit, :destroy]
   def learning
     @links = Link.where(page: 'Student Learning Resources')
   end
 
   def teaching
+    @links = Link.where(page: 'Teaching Resources')
   end
 
   def refugees
+    @links = Link.where(page: 'Refugee Backgrounders')
   end
 
   def new
     @link = Link.new
+  end
+
+  def show
+    @link = Link.find(params[:id])
   end
 
   def create
@@ -23,6 +29,29 @@ class LinksController < ApplicationController
       flash[:alert] = "We're sorry, your class has not been successfully submitted."
       render :new
     end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end      
+    else
+      flash[:alert] = "We're sorry, your link updates have not been successfully submitted."
+      render :edit
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect_to admin_path
   end
 private
   def link_params
