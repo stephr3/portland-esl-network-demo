957378867afefc4fbc7f59357f0e64bd96c3e2cd
Stephanie Gurung <stephr3@gmail.com>
2016-10-14 11:25:13 -0700

    allow admin to add a link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_filter :is_admin, only:[:new]
   def learning
+    @links = Link.all
   end
 
   def teaching
@@ -11,5 +12,20 @@ class LinksController < ApplicationController
 
   def new
     @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      flash[:notice] = "You have successfully submitted a link!"
+      redirect_to admin_path
+    else
+      flash[:alert] = "We're sorry, your class has not been successfully submitted."
+      render :new
+    end
+  end
+private
+  def link_params
+    params.require(:link).permit(:page, :title, :url, :description, :tag)
   end
 end
